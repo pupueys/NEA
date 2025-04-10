0983ffbd1c538899c681a91fa1abcda9bd6c0b54
Nadeen005 <nper0081@uni.sydney.edu.au>
2025-04-10 23:54:28 +1000

    Update timer.c

--- digitalIO/Src/timer.c
+++ digitalIO/Src/timer.c
@@ -5,21 +5,21 @@
 volatile uint8_t led_change_flag = 0;
 volatile uint8_t button_pressed_flag = 0;
 
-void enable_button_interrupt() {
-	// Disable the interrupts while messing around with the settings
-	//  otherwise can lead to strange behaviour
-	__disable_irq();
+void enable_timer_interrupt() {
+    // Disable the interrupts while messing around with the settings
+    //  otherwise can lead to strange behaviour
+    __disable_irq();
     // Configure Timer 3 for periodic interrupt
-    TIM3->PSC = 16000 - 1; // Prescaler for 1ms ticks (assuming 16 MHz clock)
-    TIM3->ARR = 1000 - 1; // Auto-reload for 500ms interval (every 500ms)
-    TIM3->DIER |= TIM_DIER_UIE; // Enable update interrupt
-    TIM3->CR1 |= TIM_CR1_CEN; // Start the timer
-	NVIC_SetPriority(TIM3_IRQn, 2);  // Set Priority
+    TIM3->PSC = 16000 - 1;              // Prescaler for 1ms ticks
+    TIM3->ARR = 1000 - 1;               // reload every 1000ms
+    TIM3->DIER |= TIM_DIER_UIE;         // Enable update interrupt
+    TIM3->CR1 |= TIM_CR1_CEN;           // Start the timer
+    NVIC_SetPriority(TIM3_IRQn, 2);     // Set Priority as no 2
 
-    NVIC_EnableIRQ(TIM3_IRQn);  // Enable Timer 3 interrupt
+    NVIC_EnableIRQ(TIM3_IRQn);          // Enable Timer 3 interrupt
 
-	// Re-enable all interrupts (now that we are finished)
-	__enable_irq();
+    // Re-enable all interrupts 
+    __enable_irq();
 }
 
 void TIM3_IRQHandler(void) {
@@ -27,8 +27,8 @@ void TIM3_IRQHandler(void) {
         TIM3->SR &= ~TIM_SR_UIF;
 
         if (button_pressed_flag) {
-            led_change_flag = 1;
-            button_pressed_flag = 0;  // Clear the flag after use
+            led_change_flag = 1;        // Set led change flag to high so that when button is pressed the LED pattern can change
+            button_pressed_flag = 0;    // Clear the button pressed flag so that it only switches during next press
         }
     }
 }
