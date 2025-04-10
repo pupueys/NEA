cb00da3d573d2a970f804e2cecb6bb0ca333e3b1
Nadeen005 <nper0081@uni.sydney.edu.au>
2025-04-10 23:49:00 +1000

    Update main.c

--- digitalIO/Src/main.c
+++ digitalIO/Src/main.c
@@ -1,13 +1,14 @@
 #include "digital_io.h"
 #include <stdint.h>
 #include "stm32f303xc.h"
+#include "set_get_leds.h"
 #include "timer.h"
 
 int main(void)
 {
-    //set_led_state(3);                          //uncomment this to test setting the LED state for part 3
-	digital_io(&chase_led);                   //Address of the callback function is passed on the initialisation function
+        //set_led_state(3);                       // uncomment this to test setting the LED state for part 3
+	digital_io(&chase_led);                   // Address of the callback function is passed on the initialisation function
 
 
-	for(;;) {}
+	for(;;) {}                                // loop forever
 }
