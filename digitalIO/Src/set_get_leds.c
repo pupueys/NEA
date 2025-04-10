f0a7f3fcdb9c572afea0c89bc8868f3a22910c05
Nadeen005 <nper0081@uni.sydney.edu.au>
2025-04-10 23:51:12 +1000

    Update set_get_leds.c

--- digitalIO/Src/set_get_leds.c
+++ digitalIO/Src/set_get_leds.c
@@ -3,15 +3,15 @@
 
 
 // LED state set to static so that it is only accessible in this file
-static volatile uint8_t led_state = 1;
+static volatile uint8_t led_state = 1;                 //initially set to turn the 1st LED on
 
 // Get the current LED state
 uint8_t get_led_state(void) {
-	return led_state;                         // return the current led state stored in led_state
+	return led_state;                              // return the current led state stored in led_state
 }
 
 // Set the LED state manually
-void set_led_state(uint8_t state) {                // pass in a uint8_t value which represents the LED state
+void set_led_state(uint8_t state) {                    // pass in a uint8_t value which represents the LED state
 	led_state = state;                             // set the input of the function to the led_state
 	*((uint8_t*)&(GPIOE->ODR) + 1) = led_state;    // update the LED state to the ODR
 }
