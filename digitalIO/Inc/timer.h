6abf0d51f79c712c7708a6875eac0d0b6e1642c0
Nadeen005 <nper0081@uni.sydney.edu.au>
2025-04-15 22:29:55 +1000

    Update timer.h

--- digitalIO/Inc/timer.h
+++ digitalIO/Inc/timer.h
@@ -1,13 +1,18 @@
+/*
+ * timer.h
+ *
+ *  Created on: Apr 9, 2025
+ *      Author: nadeen
+ */
+
 #ifndef TIMER_H_
 #define TIMER_H_
 
-//Define all headers
-
-void enable_button_interrupt() ;
+void enable_timer_interrupt() ;
 void TIM3_IRQHandler(void) ;
 
+// Use extern because we already defined these in another file
 extern volatile uint8_t led_change_flag;
-
 extern volatile uint8_t button_pressed_flag;
 
-#endif
+#endif /* TIMER_H_ */
