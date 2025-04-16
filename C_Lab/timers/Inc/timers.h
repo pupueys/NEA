b83a729a333caeb2ad6b6618284bcb3b825ee96d
anikan-k <anikan.khandhar@gmail.com>
2025-04-16 12:16:17 +1000

    Update and rename tasks.h to timers.h: Added comments for different tasks

--- C_Lab/timers/Inc/timers.h
+++ C_Lab/timers/Inc/timers.h
@@ -1,19 +1,22 @@
-#ifndef TASKS_H
-#define TASKS_H
+#ifndef TIMERS_H
+#define TIMERS_H
 #include <stdint.h>
-
 
 
 // Part a functions
 void Timer_Init(uint32_t interval_ms, void (*callback)(void));
-void Timer_Update(void);
+void Timer_Start(void);
 
 // Part b functions
 void Timer_SetPeriod(uint32_t new_interval_ms);
 uint32_t Timer_GetPeriod(void);
 
-// Part c functions
+// Part c function
 void Timer_OneShot(uint32_t delay_ms, void (*callback)(void));
 
 
-#endif // MY_TIMER_H
+// Interrupt handler
+void TIM2_IRQHandler(void);
+
+
+#endif
